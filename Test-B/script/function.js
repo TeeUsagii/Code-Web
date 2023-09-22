fcae41e183a586f8b7753b9933730f33cdc9d2e5
Tee Usagii <tiennguyen2003.b@gmail.com>
2023-09-22 11:05:51 +0700

    fix code and add like-item

--- Test-B/script/function.js
+++ Test-B/script/function.js
@@ -24,14 +24,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+//Yeu thich
+document.addEventListener('DOMContentLoaded', function () {
+  const button = document.querySelector('.red-heart');
+  let isRed = false;
+
+  button.addEventListener('click', () => {
+    if (isRed) {
+      button.style.color = 'black';
+      isRed = false;
+    } else {
+      button.style.color = 'red';
+      isRed = true;
+    }
+  });
+
+});
+
 // Mua hang
 function xacNhanMuaHang() {
   const xacNhan = confirm("Xác nhận mua hàng?");
   if (xacNhan) {
-      alert("Cảm ơn quý khách! Quý khách đã đặt hàng thành công.");
-      window.location.href = "https://humg.edu.vn/";
+    alert("Cảm ơn quý khách! Quý khách đã đặt hàng thành công.");
+    window.location.href = "https://humg.edu.vn/";
   } else {
-      alert("Chưa đặt hàng thành công.");
+    alert("Chưa đặt hàng thành công.");
   }
 }
 
@@ -39,8 +56,8 @@ function xacNhanMuaHang() {
 function xacNhanThemHang() {
   const xacNhan = confirm("Xác nhận thêm hàng?");
   if (xacNhan) {
-      alert("Cảm ơn quý khách! Quý khách đã thêm vào giỏ hàng thành công.");
+    alert("Cảm ơn quý khách! Quý khách đã thêm vào giỏ hàng thành công.");
   } else {
-      alert("Chưa thêm hàng vào giỏ.");
+    alert("Chưa thêm hàng vào giỏ.");
   }
 }
